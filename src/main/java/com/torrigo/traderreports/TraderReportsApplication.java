9e5cbec6166468fa75a90b210712020ea8ea27b4
Fernando Torrigo <torrigofernando@gmail.com>
2023-11-26 16:16:52 +0000

    first commit

---
+++ src/main/java/com/torrigo/traderreports/TraderReportsApplication.java
@@ -0,0 +1,13 @@
+package com.torrigo.traderreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderReportsApplication.class, args);
+	}
+
+}
